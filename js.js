b302590660f1ca891745b479551adce69b4764e5
Grant Hubbard <gdhubbar@purdue.edu>
2025-01-01 22:18:46 -0500

    made the grid, added functionality for first row.

---
+++ js.js
@@ -0,0 +1,88 @@
+const container = document.querySelector("#container");
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+const div4 = document.createElement("div");
+const div5 = document.createElement("div");
+const div6 = document.createElement("div");
+const div7 = document.createElement("div");
+const div8 = document.createElement("div");
+const div9 = document.createElement("div");
+const div10 = document.createElement("div");
+const div11 = document.createElement("div");
+const div12 = document.createElement("div");
+const div13 = document.createElement("div");
+const div14 = document.createElement("div");
+const div15 = document.createElement("div");
+const div16 = document.createElement("div");
+
+
+div1.setAttribute("id", "cell-0-0");
+div2.setAttribute("id", "cell-0-1");
+div3.setAttribute("id", "cell-0-2");
+div4.setAttribute("id", "cell-0-3");
+div5.setAttribute("id", "cell-1-0");
+div6.setAttribute("id", "cell-1-1");
+div7.setAttribute("id", "cell-1-2");
+div8.setAttribute("id", "cell-1-3");
+div9.setAttribute("id", "cell-2-0");
+div10.setAttribute("id", "cell-2-1");
+div11.setAttribute("id", "cell-2-2");
+div12.setAttribute("id", "cell-2-3");
+div13.setAttribute("id", "cell-3-0");
+div14.setAttribute("id", "cell-3-1");
+div15.setAttribute("id", "cell-3-2");
+div16.setAttribute("id", "cell-3-3");
+
+
+container.appendChild(div1);
+container.appendChild(div2);
+container.appendChild(div3);
+container.appendChild(div4);
+container.appendChild(div5);
+container.appendChild(div6);
+container.appendChild(div7);
+container.appendChild(div8);
+container.appendChild(div9);
+container.appendChild(div10);
+container.appendChild(div11);
+container.appendChild(div12);
+container.appendChild(div13);
+container.appendChild(div14);
+container.appendChild(div15);
+container.appendChild(div16);
+
+
+/*hover time */
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div2.onmouseenter = () => div2.style.backgroundColor = 'black';
+div2.onmouseout = () => div2.style.backgroundColor = 'aquamarine';
+div3.onmouseenter = () => div3.style.backgroundColor = 'black';
+div3.onmouseout = () => div3.style.backgroundColor = 'aquamarine';
+div3.onmouseenter = () => div3.style.backgroundColor = 'black';
+div4.onmouseout = () => div4.style.backgroundColor = 'aquamarine';
+div4.onmouseenter = () => div4.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
+div1.onmouseenter = () => div1.style.backgroundColor = 'black';
+div1.onmouseout = () => div1.style.backgroundColor = 'aquamarine';
